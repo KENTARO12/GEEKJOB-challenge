8768e0b88756c9e50e104598d85c26f8635158db
EXP-0013\guest1Day <janthi20110623@gmail.com>
2018-06-21 17:50:19 +0900

    Java課題　/標準クラス/文字数の取得　コミットします

---
+++ 015.標準クラス/GettingLength.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class GettingLength {
+    public static void main(String[] args){
+        String mail = "bayernmunich2018＠yahoomail.com";
+        System.out.println(mail.substring(16).length());
+    }
+}
