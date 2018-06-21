1f77b0ef1613bfac0e51701665331fe354df8a89
EXP-0013\guest1Day <janthi20110623@gmail.com>
2018-06-21 17:45:20 +0900

    Java課題　/標準クラス/タイムスタンプの作成　コミットします

---
+++ 015.標準クラス/MakingTimeStamp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage;
+
+import java.util.Date;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class MakingTimeStamp {
+    public static void main(String[] args){
+        Date someDay = new Date(1451574000000L);
+        System.out.println(someDay);
+    }
+}
